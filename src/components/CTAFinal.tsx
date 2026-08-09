c4a6e5374d1fd4a0e417f60325bc2351670524ca
Majoi7 <jehovaly7@gmail.com>
2026-08-09 14:42:02 +0200

    Achitecture du projet

--- src/components/CTAFinal.tsx
+++ src/components/CTAFinal.tsx
@@ -1,4 +1,5 @@
-import { useState, FormEvent } from "react";
+import { useState } from "react";
+import type { FormEvent } from "react";
 
 export default function CTAFinal() {
   const [prenom, setPrenom] = useState("");
